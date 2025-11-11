32a4e251ad4e512661c34488bca269e9ae5f6855
Dalk <daniel.demonteverde@datatalk.it>
2025-11-11 11:39:54 +0100

    readded tutti

--- components/HeaderBereBene.vue
+++ components/HeaderBereBene.vue
@@ -151,7 +151,8 @@ const items = [
   { id: 'rossi', label: 'Rossi', to: '/classifica-vini-2026/vini/rossi' },
   { id: 'bollicine', label: 'Bollicine', to: '/classifica-vini-2026/vini/bollicine' },
   { id: 'rosati', label: 'Rosati', to: '/classifica-vini-2026/vini/rosati' },
-  { id: 'vini-dolci', label: 'Vini Dolci', to: '/classifica-vini-2026/vini/vini-dolci' }
+  { id: 'vini-dolci', label: 'Vini Dolci', to: '/classifica-vini-2026/vini/vini-dolci' },
+  { id: 'tutti', label: 'Tutti', to: '/classifica-vini-2026/vini/tutti' },
 ]
 
 const isWineDetailPage = computed(() => {
