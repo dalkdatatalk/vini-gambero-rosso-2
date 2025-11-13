f33859813f478d05284b5ed41b643b5b2bd58176
Dalk <daniel.demonteverde@datatalk.it>
2025-11-13 13:50:23 +0100

    Add HeaderGeneral component

---
+++ components/HeaderGeneral.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header-general">
+    <div class="header-general__logo">
+      <img src="/img/logo-gambero-rosso-sm.png" alt="Gambero Rosso" />
+    </div>
+    <div class="header-general__logo">
+      <img src="/img/logo-bere-bene-sm.png" alt="Berebene" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header-general {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 4rem;
+}
+
+.header-general__logo img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+</style>
