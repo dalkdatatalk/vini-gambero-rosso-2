669ee3ac7484cfbc9701df6389a481768221cef8
Dalk <daniel.demonteverde@datatalk.it>
2025-11-15 15:39:28 +0100

    Adjust desktop header visibility for breakpoints

--- pages/classifica-vini-2026/vini/index.vue
+++ pages/classifica-vini-2026/vini/index.vue
@@ -1,6 +1,6 @@
 <template>
   <HeaderMobile v-if="isMobile || isTablet" />
-  <HeaderGeneral v-if="!isMobile && !isTablet" />
+  <HeaderGeneral v-if="!(isMobile || isTablet)" />
   <main class="page">
     <WineTypeFilters v-model="typeSelection" class="page__filters" />
 
